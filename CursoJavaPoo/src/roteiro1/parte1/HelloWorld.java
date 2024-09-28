be2991a3fae02ad288b03f23094369ce956f5069
Lucas Oliveira <lucas_7tv@hotmail.com>
2024-09-28 10:12:43 -0300

    simples modificação

--- CursoJavaPoo/src/roteiro1/parte1/HelloWorld.java
+++ CursoJavaPoo/src/roteiro1/parte1/HelloWorld.java
@@ -2,6 +2,7 @@ package roteiro1.parte1;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World !");
+        System.out.println("Hello, World!");
+        System.out.println("Tudo Bem?");
     }
 }
